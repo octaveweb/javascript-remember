c4bf3171eb6affdd3205b943ff55f0397717accd
octaveweb <karanswarnakar@gmail.com>
2025-02-11 12:37:49 +0530

    Data Types

---
+++ 01_basice/02_dataTypes.js
@@ -0,0 +1,42 @@
+"use strict" // treat all JS code as newr version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3); //10/10
+
+
+{
+// not good practice code excute  0/10
+console.log(3 
+    + 3); 
+}
+
+
+// Data Type 
+
+let name = "Karan Swarnakar" ;// String
+
+let agr = 20; // Number 
+
+let isLoging = false;
+
+// let state = null; // empty or null 
+
+let state; // undefined value
+
+
+
+// number  => 2 to power 53
+// bigint 
+// string => ""
+// boolean = true / false
+// null => stand allown value    
+// undefined => not defined
+// symbole => unique
+
+
+// Object 
+
+console.log(typeof null); // type of null is a object
+console.log(typeof undefined); // type of undefined is a undefined 
+
