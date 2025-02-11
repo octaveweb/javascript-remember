dca63432bd33447569bb266944da1db5163be4d8
octaveweb <karanswarnakar@gmail.com>
2025-02-11 13:37:22 +0530

    learn Operation

---
+++ 01_basice/04_Operation.js
@@ -0,0 +1,35 @@
+let value = 3;
+let negtiveValue = -value;
+// console.log(value);
+// console.log(negtiveValue);
+
+// console.log(30+ 20 );
+// console.log(30- 20 );
+// console.log(30* 20 );
+// console.log(30**20 );
+// console.log(30 / 15 );
+// console.log(30 % 20 );
+
+let str1 = "Karan";
+let str2 = " Swarnakar"
+let str3 = str1 + str2
+// console.log(str3);
+
+/*
+console.log("1" + 2); // print : {12}
+console.log(1 + "2"); // print : {12}
+console.log("1" + "2"); // print : {12}
+console.log("1" + 2 + 2); // print : {122}
+console.log(1 + 2 + "2"); // print : {32}
+*/
+console.log(+true); // increment 1+ ptint {1}
+// console.log(true+);// throw error
+console.log(+"");// {0}
+
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2+2;
+console.table([num1,num2,num3]);
+
+
